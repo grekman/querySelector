b492fffd538635e0341fd790107a460cb38faf05
grekman <grekman@gmail.com>
2017-05-05 17:06:41 +0300

    mouse and arrowkeys control v0.1.1

--- js.js
+++ js.js
@@ -1,16 +1,31 @@
 
 function  f1(event) {
-  console.log(event);
-  // this.innerHTML = event.offsetX + ' ' +event.offsetY;
-  // this.innerHTML = event.offsetY;
-
+  // console.log(event);
   document.querySelector('.tracker').style.left = event.offsetX + 5 + 'px';
   document.querySelector('.tracker').style.top = event.offsetY + 5 + 'px';
 }
 
-function f2() {
-  alert('222222');
+var x = 1;
+var y = 1;
+function f2(event) {
+  // console.log(event);
+  if (event.key=='ArrowRight' && x<=77) {
+    x=x+1;
+  }
+  else if (event.key=='ArrowLeft' && x>=1) {
+    x=x-1;
+  }
+  else if (event.key=='ArrowUp' && y>=1 ) {
+    y=y-1;
+  }
+  else if (event.key=='ArrowDown' && y<=77) {
+    y=y+1;
+  }
+
+  // console.log(x+" "+ y);
+  document.querySelector('.hero').style.left = x + 'vw';
+  document.querySelector('.hero').style.top = y + 'vh';
 }
 
-
+document.addEventListener('keydown', f2)
 document.querySelector('.test').addEventListener('mousemove',f1);
